2605df834616a6e8984eb8fbef7e80e94f195c68
Esteban Munoz <esteban.munozal@gmail.com>
2022-05-23 18:30:15 -0500

    Delete run.rb

--- run.rb
+++
@@ -1,9 +0,0 @@
-require './book'
-require './label'
-def run
-  book = Book.new('publisher 1', 'hey cover', '10/19/2022')
-  p book
-  label = Label.new('title1', 'color1')
-  label.add_item(book)
-  p label
-end
