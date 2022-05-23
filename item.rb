0273a8fffc7a03dfd2f1761e8c5c73cdd2c508f8
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-05-23 16:47:05 -0300

    Add methods visible in the diagram

--- item.rb
+++ item.rb
@@ -1,6 +1,23 @@
 class Item
+  attr_reader :genre
+
   def initialize(publish_date)
     @id = rand(1..1000)
     @publish_date = publish_date
+  end
+
+  def add_genre(genre)
+    @genre = genre
+    genre.add_item(self) unless genre.items.include?(self)
+  end
+
+  def add_author(author)
+    @author = author
+    author.add_item(self) unless author.items.include?(self)
+  end
+
+  def add_label(label)
+    @label = label
+    label.add_item(self) unless label.items.include?(self)
   end
 end
