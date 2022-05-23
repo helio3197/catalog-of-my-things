a59a71f71d2b42261977c5526fdbce361798c674
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-05-23 18:54:15 -0300

    Add MusicAlbum properties

--- music_album.rb
+++ music_album.rb
@@ -1,4 +1,10 @@
 require './item'
 
 class MusicAlbum < Item
+  attr_reader :on_spotify
+
+  def initialize(on_spotify, publish_date)
+    super(id, publish_date, archived)
+    @on_spotify = on_spotify
+  end
 end
