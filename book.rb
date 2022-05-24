095133cce31bb2eeacdd2550aa836c122b8747f4
Estebanmual <esteban.munozal@gmail.com>
2022-05-24 11:42:00 -0500

    Add initialize parameter to books

--- book.rb
+++ book.rb
@@ -3,8 +3,8 @@ require './item'
 class Book < Item
   attr_reader :publisher, :cover_state
 
-  def initialize(publisher, cover_state, publish_date)
-    super(publish_date)
+  def initialize(publisher, cover_state, publish_date, archived, id = rand(1000))
+    super(publish_date, archived: archived)
     @id = id
     @publisher = publisher
     @cover_state = cover_state
