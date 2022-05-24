624ac4f430183adb11979e0038ba54e1e5e62fd5
Kenny <kennysalazar3197@gmail.com>
2022-05-24 10:03:11 -0500

    Update Item and Author classes

--- author.rb
+++ author.rb
@@ -1,10 +1,10 @@
 class Author
   attr_reader :first_name, :last_name, :id, :items
 
-  def initialize(first_name, last_name, id = nil)
+  def initialize(first_name, last_name, id = rand(1..1000))
     @first_name = first_name
     @last_name = last_name
-    @id = id || rand(1..1000)
+    @id = id
     @items = []
   end
 
