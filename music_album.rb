8f1e8cf2b2316d66213c6e3a5477ff05cca44c00
Lucas Bonnefon <ryxtor@outlook.com>
2022-05-23 22:45:25 -0300

    Update music_album.rb
    Co-authored-by: Kenny Salazar <kennysalazar3197@gmail.com>

--- music_album.rb
+++ music_album.rb
@@ -4,7 +4,7 @@ class MusicAlbum < Item
   attr_reader :on_spotify
 
   def initialize(on_spotify, publish_date)
-    super(id, publish_date, archived)
+    super(publish_date)
     @on_spotify = on_spotify
   end
 
