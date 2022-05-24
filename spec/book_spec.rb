69f2f18041b50dd811c0a5017d793d88c209971a
Estebanmual <esteban.munozal@gmail.com>
2022-05-24 12:02:59 -0500

    Repair test due to class changes

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -2,7 +2,7 @@ require './book'
 
 describe Book do
   before :each do
-    @book = Book.new('publisher 1', 'coverState', '2010-09-18')
+    @book = Book.new('publisher 1', 'coverState', '2010-09-18', true)
   end
 
   it 'Return a book' do
@@ -15,5 +15,13 @@ describe Book do
 
   it 'Book should have a cover state' do
     expect(@book.cover_state).to eq('coverState')
+  end
+
+  it 'Book should be not archived' do
+    expect(@book.archived).to eq(true)
+  end
+
+  it 'Book should have a publish_date' do
+    expect(@book.publish_date.strftime("%Y-%m-%d")).to eq('2010-09-18')
   end
 end
