4c64a219ec095db01fe172fd8a4f912fbf586cc2
Estebanmual <esteban.munozal@gmail.com>
2022-05-25 14:48:36 -0500

    Fixed some bugs

--- label_methods.rb
+++ label_methods.rb
@@ -40,7 +40,10 @@ module LabelMethods
       list_labels
       print '> '
       selection = gets.chomp
-      create_label if selection == '0'
+      if selection == '0'
+        create_label
+        select_label
+      end
       invalid?(selection, @labels.length) ? print("Invalid option, try again.\n\n") : break
     end
     @labels[selection.to_i - 1]
